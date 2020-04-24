f614a72ccf28df501508da7ade8afc05dd6a0087
Cannon Sloan <carcanken@gmail.com>
2020-04-24 18:50:30 -0500

    Added the acceptPlayer(playerID) method from the joinMatch.

--- src/sample/Match.java
+++ src/sample/Match.java
@@ -1,15 +1,30 @@
 package sample;
+import java.util.ArrayList;
 
-public class Match {
+public class Match extends matchController {
+
     public boolean matchStatus;
     public int matchID;
     public static int matchCounter = 0;
-public Match() {
-    matchID = matchCounter;
-    matchStatus = true;
-    matchCounter++;
-}
+    public ArrayList<Account> playerList;
+
+    public Match() {
+        matchID = matchCounter;
+        matchStatus = true;
+        matchCounter++;
+    }
+
     public boolean checkMatchStatus() {
         return matchStatus;
+    }
+
+    // Goes through the process of actually adding a player to the match. First communicates with the matchController to see
+    // if player is valid.
+    public boolean acceptPlayer(Account player) {
+        if (matchController.joinMatch(player)) {
+            playerList.add(player);
+            return true;
+        }
+        return false;
     }
 }
